0bf2258dc79c2b5be82833206172f765748c9e62
Rohit Reddy Musukudabbidi <104376327+RohitReddyy@users.noreply.github.com>
2024-10-20 12:39:56 -0500

    Update Teacherlogin.js

--- frontend/src/Components/Teacherlogin.js
+++ frontend/src/Components/Teacherlogin.js
@@ -9,7 +9,7 @@ function TeacherLogin() {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch("https://realtime-polling-api.vercel.app/api/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
